aa466052b879f6919666b3aadbb83aa7ae5777b5
iMaxiOS <urk.maksim1@yandex.ru>
2020-06-19 12:50:33 +0300

    rename view and add new view controllers. changes info plist

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messenger
 //
 //  Created by Maxim Granchenko on 19.06.2020.
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
