4214aa9be95e728e5cc6fa8c140a60bb0fdd41e2
iMaxiOS <urk.maksim1@yandex.ru>
2020-06-19 12:35:53 +0300

    getting start

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Maxim Granchenko on 19.06.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
