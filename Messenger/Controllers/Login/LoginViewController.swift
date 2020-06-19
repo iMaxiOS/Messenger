1686ada4b79a5ff87c91c2e43256ee51e8691a27
iMaxiOS <urk.maksim1@yandex.ru>
2020-06-19 14:57:57 +0300

    impelementation log in scene and register scene

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -9,18 +9,120 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.clipsToBounds = true
+        return scroll
+    }()
+    
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "logo")
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.autocapitalizationType = .none
+        tf.returnKeyType = .continue
+        tf.keyboardType = .emailAddress
+        tf.autocorrectionType = .no
+        tf.layer.cornerRadius = 12
+        tf.layer.borderColor = UIColor.systemGray4.cgColor
+        tf.layer.borderWidth = 0.6
+        tf.placeholder = "Enter Email Address..."
+        tf.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 6, height: 0))
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.autocapitalizationType = .none
+        tf.returnKeyType = .done
+        tf.autocorrectionType = .no
+        tf.layer.cornerRadius = 12
+        tf.layer.borderColor = UIColor.systemGray4.cgColor
+        tf.layer.borderWidth = 0.6
+        tf.placeholder = "Enter Password..."
+        tf.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 6, height: 0))
+        tf.leftViewMode = .always
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private let logInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.systemBackground, for: .normal)
+        button.layer.cornerRadius = 12
+        button.clipsToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Log In"
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(logoImageView)
+        scrollView.addSubview(emailTextField)
+        scrollView.addSubview(passwordTextField)
+        scrollView.addSubview(logInButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        logoImageView.frame = CGRect(x: (scrollView.width - size) / 2, y: 20, width: size, height: size)
+        emailTextField.frame = CGRect(x: 30, y: logoImageView.bottom + 30, width: scrollView.width - 60, height: 50)
+        passwordTextField.frame = CGRect(x: 30, y: emailTextField.bottom + 10, width: scrollView.width - 60, height: 50)
+        logInButton.frame = CGRect(x: 30, y: passwordTextField.bottom + 30, width: scrollView.width - 60, height: 50)
+    }
+    
+    @objc private func loginButtonTapped() {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+        guard let email = emailTextField.text, let password = passwordTextField.text, !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+            alertUserLoginError()
+            return
+        }
+    }
+    
+    private func alertUserLoginError() {
+        let alert = UIAlertController(title: "Woops", message: "Please enter all information to log in.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true)
     }
     
     @objc private func didTapRegister() {
         let registerVC = RegisterViewController()
         registerVC.title = "Create Account"
         navigationController?.pushViewController(registerVC, animated: true)
+    }
+}
+
+//MARK: Text Field Delegete
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            loginButtonTapped()
+        }
+        return true
     }
 }
--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -9,9 +9,172 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.clipsToBounds = true
+        scroll.isUserInteractionEnabled = true
+        return scroll
+    }()
+    
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "person")
+        iv.isUserInteractionEnabled = true
+        iv.tintColor = .systemGray
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    private let firstNameTextField: UITextField = {
+        let tf = UITextField()
+        tf.autocapitalizationType = .none
+        tf.returnKeyType = .continue
+        tf.autocorrectionType = .no
+        tf.layer.cornerRadius = 12
+        tf.layer.borderColor = UIColor.systemGray4.cgColor
+        tf.layer.borderWidth = 0.6
+        tf.placeholder = "Enter First Name..."
+        tf.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 6, height: 0))
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
+    private let lastNameTextField: UITextField = {
+        let tf = UITextField()
+        tf.autocapitalizationType = .none
+        tf.returnKeyType = .continue
+        tf.autocorrectionType = .no
+        tf.layer.cornerRadius = 12
+        tf.layer.borderColor = UIColor.systemGray4.cgColor
+        tf.layer.borderWidth = 0.6
+        tf.placeholder = "Enter Last Name..."
+        tf.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 6, height: 0))
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.autocapitalizationType = .none
+        tf.returnKeyType = .continue
+        tf.keyboardType = .emailAddress
+        tf.autocorrectionType = .no
+        tf.layer.cornerRadius = 12
+        tf.layer.borderColor = UIColor.systemGray4.cgColor
+        tf.layer.borderWidth = 0.6
+        tf.placeholder = "Enter Email Address..."
+        tf.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 6, height: 0))
+        tf.leftViewMode = .always
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.autocapitalizationType = .none
+        tf.returnKeyType = .done
+        tf.autocorrectionType = .no
+        tf.layer.cornerRadius = 12
+        tf.layer.borderColor = UIColor.systemGray4.cgColor
+        tf.layer.borderWidth = 0.6
+        tf.placeholder = "Enter Password..."
+        tf.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 6, height: 0))
+        tf.leftViewMode = .always
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Register", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.systemBackground, for: .normal)
+        button.layer.cornerRadius = 12
+        button.clipsToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPurple
+        title = "Log In"
+        view.backgroundColor = .systemBackground
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapChangePic))
+        gesture.numberOfTouchesRequired = 1
+        gesture.numberOfTapsRequired = 1
+        logoImageView.addGestureRecognizer(gesture)
+        
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(logoImageView)
+        scrollView.addSubview(firstNameTextField)
+        scrollView.addSubview(lastNameTextField)
+        scrollView.addSubview(emailTextField)
+        scrollView.addSubview(passwordTextField)
+        scrollView.addSubview(registerButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        logoImageView.frame = CGRect(x: (scrollView.width - size) / 2, y: 20, width: size, height: size)
+        firstNameTextField.frame = CGRect(x: 30, y: logoImageView.bottom + 30, width: scrollView.width - 60, height: 50)
+        lastNameTextField.frame = CGRect(x: 30, y: firstNameTextField.bottom + 10, width: scrollView.width - 60, height: 50)
+        emailTextField.frame = CGRect(x: 30, y: lastNameTextField.bottom + 10, width: scrollView.width - 60, height: 50)
+        passwordTextField.frame = CGRect(x: 30, y: emailTextField.bottom + 10, width: scrollView.width - 60, height: 50)
+        registerButton.frame = CGRect(x: 30, y: passwordTextField.bottom + 30, width: scrollView.width - 60, height: 50)
+    }
+    
+    @objc private func didTapChangePic() {
+        print("tap")
+    }
+    
+    @objc private func registerButtonTapped() {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        firstNameTextField.resignFirstResponder()
+        lastNameTextField.resignFirstResponder()
+
+        guard let firstName = firstNameTextField.text, let lastName = lastNameTextField.text, let email = emailTextField.text, let password = passwordTextField.text, !firstName.isEmpty, !lastName.isEmpty, !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+            alertUserLoginError()
+            return
+        }
+    }
+    
+    private func alertUserLoginError() {
+        let alert = UIAlertController(title: "Woops", message: "Please enter all information to create a new account.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        present(alert, animated: true)
+    }
+    
+    @objc private func didTapRegister() {
+        let registerVC = RegisterViewController()
+        registerVC.title = "Create Account"
+        navigationController?.pushViewController(registerVC, animated: true)
+    }
+}
+
+//MARK: Text Field Delegete
+extension RegisterViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == firstNameTextField {
+            lastNameTextField.becomeFirstResponder()
+        } else if textField == lastNameTextField {
+            emailTextField.becomeFirstResponder()
+        } else if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            registerButtonTapped()
+        }
+        return true
     }
 }
