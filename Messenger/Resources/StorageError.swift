5f8af903d673f1135582c71ea9c53ab6be9bb8ed
iMaxiOS <urk.maksim1@yandex.ru>
2020-06-26 15:14:16 +0300

    add storage manager and storage error

---
+++ Messenger/Resources/StorageError.swift
@@ -0,0 +1,14 @@
+//
+//  StorageError.swift
+//  Messenger
+//
+//  Created by Maxim Granchenko on 26.06.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import Foundation
+
+enum StorageError: Error {
+    case failedToUpload
+    case fiiledToDownloadUrl
+}
---
+++ Messenger/Resources/StorageManager.swift
@@ -0,0 +1,40 @@
+//
+//  StorageManager.swift
+//  Messenger
+//
+//  Created by Maxim Granchenko on 26.06.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import Foundation
+import FirebaseStorage
+
+final class StorageManager {
+
+    typealias UploadPictureCompletion = ((Result<String, Error>) -> Void)
+    static let shared = StorageManager()
+    private let storage = Storage.storage().reference()
+    
+    /// Upload picture to firebase storage and completion with url to download
+    public func uploadProfilePicture(with data: Data, fileName: String, completed: @escaping UploadPictureCompletion) {
+        storage.child("images/\(fileName)").putData(data, metadata: nil) { metadata, error in
+            guard error == nil else {
+                print("Failed to upload data to firebase for picture")
+                completed(.failure(StorageError.failedToUpload))
+                return
+            }
+            
+            self.storage.child("images/\(fileName)").downloadURL { url, error in
+                guard let url = url else {
+                    print("Failed to get download to url")
+                    completed(.failure(StorageError.fiiledToDownloadUrl))
+                    return
+                }
+
+                let urlString = url.absoluteString
+                print("download url returned: \(urlString)")
+                completed(.success(urlString))
+            }
+        }
+    }
+}
